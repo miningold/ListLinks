b91a17601053e43b0fd34882c3782465d01d63a0
Brian Kopleck <bkopleck@gmail.com>
2013-03-02 18:32:15 -0800

    add favicon

--- app.js
+++ app.js
@@ -64,7 +64,7 @@ app.configure(function() {
   app.set('port', process.env.VMC_APP_PORT || 3000);
   app.set('views', __dirname + '/views');
   app.set('view engine', 'hbs');
-  app.use(express.favicon());
+  app.use(express.favicon(__dirname + '/public/favicon.png'));
   app.use(express.logger('dev'));
   app.use(express.bodyParser());
   app.use(express.methodOverride());
